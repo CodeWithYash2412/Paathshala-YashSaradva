8a15be04b1e7d67988aa796fcf1bc0c4688b04b7
CodeWithYash2412 <84624661+CodeWithYash2412@users.noreply.github.com>
2023-07-25 22:52:11 +0530

    Some Changes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { Amplify } from "aws-amplify"
+import awsconfig from "./aws-exports"
+
+Amplify.configure(awsconfig)
 
 createApp(App).mount('#app')
